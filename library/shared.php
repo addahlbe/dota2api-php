f0033265b9f206ab50c14ff642e0ccd4dade5ba2
Joe Dallago <jd.dallago@gmail.com>
2012-10-04 04:34:49 +0000

    Have the tutorial done up to the Template class.

--- library/shared.php
+++ library/shared.php
@@ -3,15 +3,15 @@
 /** Check if environment is development and display errors **/
 
 function setReporting() {
-if (DEVELOPMENT_ENVIRONMENT == true) {
-    error_reporting(E_ALL);
-    ini_set('display_errors','On');
-} else {
-    error_reporting(E_ALL);
-    ini_set('display_errors','Off');
-    ini_set('log_errors', 'On');
-    ini_set('error_log', ROOT.DS.'tmp'.DS.'logs'.DS.'error.log');
-}
+    if (DEVELOPMENT_ENVIRONMENT == true) {
+        error_reporting(E_ALL);
+        ini_set('display_errors','On');
+    } else {
+        error_reporting(E_ALL);
+        ini_set('display_errors','Off');
+        ini_set('log_errors', 'On');
+        ini_set('error_log', ROOT.DS.'tmp'.DS.'logs'.DS.'error.log');
+    }
 }
 
 /** Check for Magic Quotes and remove them **/
@@ -22,11 +22,11 @@ function stripSlashesDeep($value) {
 }
 
 function removeMagicQuotes() {
-if ( get_magic_quotes_gpc() ) {
-    $_GET    = stripSlashesDeep($_GET   );
-    $_POST   = stripSlashesDeep($_POST  );
-    $_COOKIE = stripSlashesDeep($_COOKIE);
-}
+    if ( get_magic_quotes_gpc() ) {
+        $_GET    = stripSlashesDeep($_GET   );
+        $_POST   = stripSlashesDeep($_POST  );
+        $_COOKIE = stripSlashesDeep($_COOKIE);
+    }
 }
 
 /** Check register globals and remove them **/
@@ -50,7 +50,7 @@ function callHook() {
     global $url;
 
     $urlArray = array();
-    $urlArray = explode("/",$url);
+    $urlArray = explode("/",$url); // in python, str.split("/")
 
     $controller = $urlArray[0];
     array_shift($urlArray);
--- public/index.php
+++ public/index.php
@@ -6,8 +6,6 @@ define('ROOT', dirname(dirname(_FILE_)));
 $url = &_GET['url'];
 
 require_once(ROOT . DS . 'library' . DS . 'bootstrap.php');
-require_once(ROOT . DS . 'config' . DS . 'config.php');
-require_once(ROOT . DS . 'library' . DS . 'shared.php');
 
 //purposely no ending php tag. This is to avoid injection of any extra whitespaces in our output.
 
